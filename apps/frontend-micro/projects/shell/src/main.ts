f3e4b0174c53c8f61ecf3be1daee5e2100f1908e
HoanIT <lehoan.dev@gmail.com>
2026-04-27 01:47:38 +0700

    update

--- apps/frontend-micro/projects/shell/src/main.ts
+++ apps/frontend-micro/projects/shell/src/main.ts
@@ -1,6 +1,16 @@
 import { initFederation } from '@angular-architects/native-federation';
 
-initFederation('federation.manifest.json')
+// Lấy tham số cấu hình từ env.js (được nạp vào runtime trước Angular)
+const env = (window as any).__env || {};
+
+// Thiết lập Map động trỏ đến các module con
+const federationManifest = {
+  iam: `${env.mfeIamUrl || 'http://localhost:3002'}/remoteEntry.json`,
+  common: `${env.mfeCommonUrl || 'http://localhost:3001'}/remoteEntry.json`
+};
+
+// Truyền thẳng Object vào initFederation (không dùng file json cứng nữa)
+initFederation(federationManifest)
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));

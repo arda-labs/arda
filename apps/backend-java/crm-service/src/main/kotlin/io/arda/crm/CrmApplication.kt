7b1a40c7e7760a6ddc3d9ececb8003b372402905
HoanIT <lehoan.dev@gmail.com>
2026-05-01 17:11:10 +0700

    fix: make spring boot application class open in kotlin

--- apps/backend-java/crm-service/src/main/kotlin/io/arda/crm/CrmApplication.kt
+++ apps/backend-java/crm-service/src/main/kotlin/io/arda/crm/CrmApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CrmApplication
+open class CrmApplication
 
 fun main(args: Array<String>) {
     runApplication<CrmApplication>(*args)

4aacd9d21220ff89cb8c4b16db25874dd955161d
HoanIT <lehoan.dev@gmail.com>
2026-05-01 16:42:01 +0700

    ci: fix gradle project naming hierarchy for java libs

--- apps/backend-java/settings.gradle.kts
+++ apps/backend-java/settings.gradle.kts
@@ -5,18 +5,18 @@ include("accounting")
 include("crm-service")
 
 // Libs
-include("libs-common")
-include("libs-database")
-include("libs-grpc-client")
-include("libs-security")
-include("libs-messaging")
+include(":libs:java:common")
+include(":libs:java:database")
+include(":libs:java:grpc-client")
+include(":libs:java:security")
+include(":libs:java:messaging")
 
 // Map to physical paths
 project(":accounting").projectDir = file("accounting")
 project(":crm-service").projectDir = file("crm-service")
-project(":libs-common").projectDir = file("../../libs/java/common")
-project(":libs-database").projectDir = file("../../libs/java/database")
-project(":libs-grpc-client").projectDir = file("../../libs/java/grpc-client")
-project(":libs-security").projectDir = file("../../libs/java/security")
-project(":libs-messaging").projectDir = file("../../libs/java/messaging")
+project(":libs:java:common").projectDir = file("../../libs/java/common")
+project(":libs:java:database").projectDir = file("../../libs/java/database")
+project(":libs:java:grpc-client").projectDir = file("../../libs/java/grpc-client")
+project(":libs:java:security").projectDir = file("../../libs/java/security")
+project(":libs:java:messaging").projectDir = file("../../libs/java/messaging")
 
